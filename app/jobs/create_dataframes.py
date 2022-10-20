51b86997c17e7892e124fce6743b57ca6deedfed
Leticia Chiu <leticiapchiu@gmail.com>
2022-10-19 23:36:36 -0300

    Configure postgres connection and create tables

--- app/jobs/create_dataframes.py
+++ app/jobs/create_dataframes.py
@@ -3,22 +3,31 @@ import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 import plotly.express as px
-# import psycopg
-# import io
-  
+
 #LECOM
-fornecedores = pd.DataFrame(pd.read_csv('app/data/fornecedores.csv', delimiter=','))
-avaliacoes = pd.DataFrame(pd.read_csv('app/data/avaliacoes.csv', delimiter=','))
+fornecedores = pd.DataFrame(pd.read_csv('./data/fornecedores.csv', delimiter=','))
+new_header = fornecedores.iloc[0]
+fornecedores = fornecedores[1:]
+fornecedores.columns = new_header
+fornecedores = fornecedores[fornecedores['Fornecedor'].notna()]
+
+avaliacoes = pd.DataFrame(pd.read_csv('./data/avaliacoes.csv', delimiter=','))
+new_header = avaliacoes.iloc[0]
+avaliacoes = avaliacoes[1:]
+avaliacoes.columns = new_header
+avaliacoes = avaliacoes[avaliacoes['Fornecedor'].notna()]
+
 fornecedores_avaliacoes = fornecedores.merge(avaliacoes,how='left',left_on='Fornecedor', right_on='Fornecedor')
 
+
 # PIRAMIDE
-recife = pd.DataFrame(pd.read_csv('app/data/fornecedores.csv', delimiter=','))
-norte = pd.DataFrame(pd.read_csv('app/data/fornecedores.csv', delimiter=','))
-sorocaba = pd.DataFrame(pd.read_csv('app/data/fornecedores.csv', delimiter=','))
-curitiba = pd.DataFrame(pd.read_csv('app/data/fornecedores.csv', delimiter=','))
+recife = pd.DataFrame(pd.read_csv('./data/fornecedores.csv', delimiter=','))
+norte = pd.DataFrame(pd.read_csv('./data/fornecedores.csv', delimiter=','))
+sorocaba = pd.DataFrame(pd.read_csv('./data/fornecedores.csv', delimiter=','))
+curitiba = pd.DataFrame(pd.read_csv('./data/fornecedores.csv', delimiter=','))
 relatorios = pd.concat([recife,norte,sorocaba,curitiba])
 
-# GRAFICO SETORES
+# DF SETORES
 frequencia_categorias = pd.DataFrame(fornecedores["Produto / Serviço"].value_counts().reset_index().values, columns=["Categoria", "Frequencia"])
 frequencia_categoriasindex = frequencia_categorias.sort_index(axis = 0, ascending=True)
 frequencia_categoriasindex = frequencia_categoriasindex.loc[(frequencia_categoriasindex.Frequencia > 4)]
--- app/jobs/database.py
+++ app/jobs/database.py
@@ -1,30 +1,41 @@
 import psycopg2
-import pandas as pd
 from sqlalchemy import create_engine
-from create_dataframes import lecom_data
-  
-conn_string = 'postgres://user:password@host/data1'
-  
+from create_dataframes import freq_categorias
+
+conn_string = 'postgres://postgres:pass@127.0.0.1/TESTE'
+
 db = create_engine(conn_string)
 conn = db.connect()
-  
-  
-# our dataframe
-data = lecom_data
-  
-# Create DataFrame
-df = pd.DataFrame(data)
-df.to_sql('data', con=conn, if_exists='replace',
-          index=False)
-conn = psycopg2.connect(conn_string
-                        )
-conn.autocommit = True
-cursor = conn.cursor()
-  
-sql1 = '''select * from data;'''
+conn1 = psycopg2.connect(
+	database="TESTE",
+user='postgres',
+password='pass',
+host='127.0.0.1',
+port= '5432'
+)
+
+conn1.autocommit = True
+cursor = conn1.cursor()
+
+cursor.execute('drop table if exists TESTE_TABLE')
+
+sql = '''CREATE TABLE TESTE_TABLE(id int ,
+Categoria char(50),Frequencia int);'''
+
+cursor.execute(sql)
+
+# importar csv
+data = freq_categorias()
+
+# converter data para sql
+data.to_sql('frequencia_categorias', conn, if_exists= 'replace')
+
+# fetching all rows
+sql1='''select * from frequencia_categorias;'''
 cursor.execute(sql1)
 for i in cursor.fetchall():
-    print(i)
-  
-# conn.commit()
-conn.close()
+	print(i)
+
+conn1.commit()
+conn1.close()
+
